a6804f556867afbb86b577350ab7aadbde4247b0
solaretech <solaretech.kk@gmail.com>
2020-08-15 16:03:30 +0900

    Edit Stores

---
+++ src/js/stores/TodoStore.js
@@ -0,0 +1,26 @@
+import { EventEmitter } from "events";
+
+class TodoStore extends EventEmmiter {
+  constructor() {
+    super();
+    this.todos = [
+      {
+        id: 113464613,
+        text: 'Go Shopping',
+        complete: false
+      },{
+        id: 235684679,
+        text: 'Pay Bills',
+        complete: false
+      }
+    ];
+  }
+
+  getAll(){
+    return this.todos;
+  }
+}
+
+const todoStore = new TodoStore
+
+export default todoStore;
